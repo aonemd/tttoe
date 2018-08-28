99f5e1ebee5ea7652fbd2f68618c957e8ead2fda
aonemd <aonemdsaleh@gmail.com>
2018-08-28 21:32:15 +0200

    Add the code that shows and moves the game cursor

--- main.c
+++ main.c
@@ -46,18 +46,39 @@ void drawBoard(Board *board) {
 			}
 
 			mvaddch(board->y+i, board->x+j, current_symbol);
+			refresh();
 		}
 	}
 }
 
+void moveCursor (int *origin, int *destination, Board *board) {
+	int i, x, y;
+	attron(COLOR_PAIR(3));
+	for (i = 0; i <= 10; i++) {
+        y = board->max_y/board->nrows * destination[0] + board->y + 5;
+        x = board->max_x/board->nrows * destination[1] + board->x + 1 + i;
+        mvaddch(y, x, ACS_CKBOARD);
+		refresh();
+	}
+	attroff(COLOR_PAIR(3));
+}
+
 int main(int argc, char *argv[]) {
 	int max_y, max_x;
-	int exit_game = 0;
-	Board *board = malloc(sizeof(Board));
+	int exit_game            = 0;
+	int cursor_origin[]      = {0, 0};
+	int cursor_destination[] = {0, 0};
+	Board *board             = malloc(sizeof(Board));
 
 	initscr();
 	noecho();
 	curs_set(FALSE);
+    keypad(stdscr, TRUE);
+    start_color();
+
+    init_pair(1, COLOR_BLUE, COLOR_BLACK);  // player 1
+    init_pair(2, COLOR_RED, COLOR_BLACK);   // player 2
+    init_pair(3, COLOR_WHITE, COLOR_BLACK); // cursor
 
 	while (!exit_game) {
 		getmaxyx(stdscr, max_y, max_x);
@@ -68,7 +89,39 @@ int main(int argc, char *argv[]) {
 		board->max_y    = board->ncolumns * 6;
 
 		drawBoard(board);
-		refresh();
+		moveCursor(cursor_origin, cursor_destination, board);
+
+		int keyPressed = getch();
+		switch (keyPressed) {
+			case KEY_UP:
+				if (cursor_origin[0] > 0) {
+					cursor_destination[0] = cursor_origin[0] - 1;
+					cursor_origin[0]      = cursor_destination[0];
+				}
+				break;
+			case KEY_DOWN:
+				if (cursor_origin[0] < board->ncolumns-1) {
+					cursor_destination[0] = cursor_origin[0] + 1;
+					cursor_origin[0]      = cursor_destination[0];
+				}
+				break;
+			case KEY_RIGHT:
+				if (cursor_origin[1] < board->nrows-1) {
+					cursor_destination[1] = cursor_origin[1] + 1;
+					cursor_origin[1]      = cursor_destination[1];
+				}
+				break;
+			case KEY_LEFT:
+				if (cursor_origin[1] > 0) {
+					cursor_destination[1] = cursor_origin[1] - 1;
+					cursor_origin[1]      = cursor_destination[1];
+				}
+				break;
+			case ' ':
+			case 'q':
+				exit_game = 1;
+
+		}
 	}
 
     resetty();
