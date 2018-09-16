2df31f15b6cc85a90e8d5646f51036f7b9f0b16f
aonemd <aonemdsaleh@gmail.com>
2018-09-16 19:54:40 +0200

    Add max and min macros

--- ttt.c
+++ ttt.c
@@ -4,6 +4,16 @@
 #include <string.h>
 #include <ncurses.h>
 
+#define max(a, b) \
+  ({ __typeof__ (a) _a = (a); \
+   __typeof__ (b) _b = (b); \
+   _a > _b ? _a : _b; })
+
+#define min(a, b) \
+  ({ __typeof__ (a) _a = (a); \
+   __typeof__ (b) _b = (b); \
+   _a < _b ? _a : _b; })
+
 # define HUMAN_PLAYER 'X'
 # define AI_PLAYER 'O'
 # define NONE '\0'
